61eb458dbd56d589b1294a81a3017677d5589c6e
Zijie Feng <fengye0907@outlook.com>
2018-10-07 14:43:20 +0200

    2

--- R/LAB5C.R
+++ R/LAB5C.R
@@ -3,6 +3,10 @@
 #' This package LAB5C contains a function using API for map searching.
 #'
 #' @docType package
+#' @author Zijie Feng
+#' @author Andreas C.charitos
+#' @author Muhammad Daud
+#'
 #'
 #' @name LAB5C
 NULL
